9dbacbf4fc18181300d4f281074a8dd9b00ccc09
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-10-05 00:10:59 +0200

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -39,7 +39,7 @@ const handler = async (m, {conn, args, usedPrefix, command}) => {
       }
     } else if (args[0]) {
       if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author, ["✨"], metadata);
-      else return m.reply(`${tradutor.texto2} ${usedPrefix}s https://telegra.ph/file/0dc687c61410765e98de2.jpg*`);
+      else return m.reply(`${tradutor.texto2} ${usedPrefix}s https://i.ibb.co/xqFc0dvP/Menu2.jpg*`);
     }
   } catch (e) {
     console.error(e);
