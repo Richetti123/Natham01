20e2fc0d1fac282d9abaf01512cf6b7afea76491
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-10-05 00:02:42 +0200

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -9,21 +9,9 @@ const handler = async (m, {conn, usedPrefix}) => {
   const text = `${tradutor.texto1[0]}
 
 ${tradutor.texto1[1]}
-1.- https://chat.whatsapp.com/LjJbmdO0qSDEKgB60qivZj
-
-2.- https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs
-
-3.- https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F
-
-4.- https://chat.whatsapp.com/CjexkGVr37J6GuSdDVAHzC
-
-> Sunlight Team  :
-
-1.- https://whatsapp.com/channel/0029Vam7yUg77qVaz3sIAp0z
-
-2.- https://chat.whatsapp.com/Fy74b6fgE9SJJpHVi6CKJY`.trim();
+- https://whatsapp.com/channel/0029Vb5KnEv0wajldteZlH0G`.trim();
   const buttonMessage= {
-    'document': {url: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`},
+    'document': {url: `https://whatsapp.com/channel/0029Vb5KnEv0wajldteZlH0G`},
     'mimetype': `application/${document}`,
     'fileName': `${tradutor.texto2}`,
     'fileLength': 99999999999999,
@@ -32,13 +20,13 @@ ${tradutor.texto1[1]}
       'forwardingScore': 200,
       'isForwarded': true,
       'externalAdReply': {
-        'mediaUrl': 'https://github.com/BrunoSobrino/TheMystic-Bot-MD',
+        'mediaUrl': 'https://whatsapp.com/channel/0029Vb5KnEv0wajldteZlH0G',
         'mediaType': 2,
         'previewType': 'pdf',
         'title': `${tradutor.texto3}`,
         'body': wm,
         'thumbnail': imagen1,
-        'sourceUrl': 'https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'}},
+        'sourceUrl': 'https://whatsapp.com/channel/0029Vb5KnEv0wajldteZlH0G'}},
     'caption': text,
     'footer': wm,
     'headerType': 6};
